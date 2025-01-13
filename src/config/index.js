b2128e3349dbe8a6101fcbca90b38471f787811a
JuanDNJ <juandevnjv@gmail.com>
2025-01-13 12:49:32 +0100

    fix: Guardando una lista para no perderla

--- src/config/index.js
+++ src/config/index.js
@@ -13,3 +13,60 @@ export const URLS_AMAZON = {
   HAMILTON: "https://amzn.to/3HqDAmU",
   SMEG: "https://amzn.to/48WcSy2",
 };
+const listDefault = [
+  {
+    title: "Bomann KM 315 Batidora amasadora 1200 W",
+    name: "km 315",
+    href: "https://amzn.to/3OiIB4C",
+    img: {
+      src: "/images/webp/bomann-km-315.webp",
+      alt: "bomann",
+    },
+  },
+  {
+    title:
+      "CREATE DOWNMIX RETRO Batidora Amasadora de rotación planetaria Blanco",
+    name: "create",
+    href: "https://amzn.to/49160Q2",
+    img: {
+      src: "/images/webp/create.webp",
+      alt: "create",
+    },
+  },
+  {
+    title: "Family Care Amasadora 1000W",
+    name: "family-care",
+    href: "https://amzn.to/493woc0",
+    img: {
+      src: "/images/webp/family-care-amasadora-1000w.webp",
+      alt: "family-care",
+    },
+  },
+  {
+    title: "Mesko MS 4217B Batidora Amasadora 1200W",
+    name: "mesko",
+    href: "https://amzn.to/48SkTEK",
+    img: {
+      src: "/images/webp/mesko.webp",
+      alt: "mesko",
+    },
+  },
+  {
+    title: "Ufesa Elite Noir Batidora Amasadora 1400W",
+    name: "ufesa",
+    href: "https://amzn.to/4bgnklS",
+    img: {
+      src: "/images/webp/ufesa.webp",
+      alt: "ufesa",
+    },
+  },
+  {
+    title: "Cecotec Batidora Amasadora Cecomixer Easy. 250 W",
+    name: "cecotec",
+    href: "https://amzn.to/3SqOoHV",
+    img: {
+      src: "/images/webp/cecotec.webp",
+      alt: "cecotec",
+    },
+  },
+];
